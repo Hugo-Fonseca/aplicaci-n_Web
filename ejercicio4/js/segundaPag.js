7bb6c690fc0c31710663361c120803c4f3b46e7d
Hugo Fonseca <hug.alexfc@gmail.com>
2024-03-16 12:00:45 -0500

    Se agrego el html y css del ejercicio 4

---
+++ ejercicio4/js/segundaPag.js
@@ -0,0 +1,4 @@
+const botonRegresar = document.getElementById('retornar');
+botonRegresar.addEventListener('click', function() {
+  window.location.href = '../index.html';
+});
--- js/general.js
+++ js/general.js
@@ -1,4 +1,7 @@
-const botonIrPagina = document.getElementById('otraPagina');
-botonIrPagina.addEventListener('click', function() {
-  window.location.href = 'ejercicio3/index.html';
+document.getElementById("pagina2").addEventListener("click", function() {
+  window.location.href = "ejercicio3/index.html";
+});
+
+document.getElementById("pagina3").addEventListener("click", function() {
+  window.location.href = "ejercicio4/index.html";
 });
